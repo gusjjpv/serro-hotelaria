ea7944848fb0ae0d8381becc02868b06ae9035ac
gusjjpv <joaogustavolima084@gmail.com>
2026-06-13 16:31:42 -0300

    build: install jwt

--- setup/settings.py
+++ setup/settings.py
@@ -121,4 +121,8 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny",
     ],
+
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
 }
