f9fbf46e0e09956e9fe1490ae16da3300133c545
gusjjpv <joaogustavolima084@gmail.com>
2026-06-14 17:43:21 -0300

    feat:model paraa user e endereco

--- controleDeAcesso/models.py
+++ controleDeAcesso/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Endereco(models.Model):    
+    rua = models.CharField(max_length=100)
+    numero = models.CharField(max_length=5)
+    complemento = models.CharField(max_length=100)
+    bairro = models.CharField(max_length=100)
+    cidade = models.CharField(max_length=100)
+    estado = models.CharField(max_length=100)
+    cep = models.CharField(max_length=8)
+
+    def __str__(self):
+        return f"{self.rua}, {self.numero} - {self.bairro}, {self.cidade} - {self.estado}, CEP: {self.cep}"
+
+class Usuario(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    login = models.CharField(max_length=20, unique=True)
+    senha = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=14, unique=True)
+    dataNascimento = models.DateField()
+    genero = models.CharField(max_length=10)
+    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
+    cpf = models.CharField(max_length=11, unique=True)
+    dataCriacao = models.DateTimeField(auto_now_add=True)
+    dataAtualizacao = models.DateTimeField(auto_now=True)
+
+    class Role(models.TextChoices):
+        SUPERVISOR = 'SV', 'Supervisor'
+        GESTOR = 'GE', 'Gestor'
+        ATENDENDE = 'AT', 'Atendente'
+        HOSPEDE = 'HO', 'Hóspede'
+    
+    role = models.CharField(max_length=2, choices=Role.choices)
+
+    def __str__(self):
+        return f"{self.login} - {self.email}"
