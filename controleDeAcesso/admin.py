526f506661d88960250e1df6641d919d6c09b193
gusjjpv <joaogustavolima084@gmail.com>
2026-06-14 17:42:30 -0300

    feat: add usuario e endereco ao dash admin

--- controleDeAcesso/admin.py
+++ controleDeAcesso/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Usuario, Endereco
 
-# Register your models here.
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('login', 'email', 'role', 'dataCriacao')
+    search_fields = ('login', 'email', 'cpf')
+    list_filter = ('role',)
+
+@admin.register(Endereco)
+class EnderecoAdmin(admin.ModelAdmin):
+    list_display = ('rua', 'numero', 'bairro', 'cidade', 'estado', 'cep')
+    search_fields = ('rua', 'bairro', 'cidade', 'estado', 'cep')
+    list_filter = ('estado', 'cidade',)
