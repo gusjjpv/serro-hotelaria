49cfc22f5ae7080d768ef3ed0794bf2ba62adbe6
gusjjpv <joaogustavolima084@gmail.com>
2026-06-17 20:08:02 -0300

    fix: criacao de gestor corrigido

--- controleDeAcesso/urls.py
+++ controleDeAcesso/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import RegisterView, CustomTokenObtainPairView, MeView, UserListCreateView, UserDetailView
+from .views import RegisterView, GestorRegisterView, CustomTokenObtainPairView, MeView, UserListCreateView, UserDetailView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='auth-register'),
+    path('register/gestor/', GestorRegisterView.as_view(), name='auth-register-gestor'),
     path('login/', CustomTokenObtainPairView.as_view(), name='auth-login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
     path('me/', MeView.as_view(), name='auth-me'),
